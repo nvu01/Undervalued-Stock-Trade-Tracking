9ebacaa6c7ab7e3eecf93e0ed7cb338fa7a9d691
nvu01 <nganvu2601@gmail.com>
2026-02-24 17:55:14 -0500

    Updated etl.py

--- etl.py
+++ etl.py
@@ -2,54 +2,24 @@ import glob
 import os
 import pandas as pd
 from io import StringIO
-import argparse
 
-# Get the filepath for the latest position statement file
-def get_latest_file (folder):
+def get_files(folder):
     '''
-    Retrieves the latest CSV file in the "position_statement" folder.
+    Retrieve all files in a folder
+    Return a list of file paths
+    Return the number files in the folder
     '''
-    files = glob.glob(os.path.join(folder, "*"))
-    filepath = max(files, key=os.path.getmtime)
-    return filepath
 
-# Transform the current position data
-def get_current_pos (filepath):
-    '''
-    Processes the current position data downloaded from TOS.
-    Parameter is the filepath to the most current CSV file in "position_statement" folder.
-    1. Load the content of the CSV file.
-    2. Find and extract the table named "Undervalued"
-    3. Save the table as "current_pos.csv" in the parent folder
-    Return the table as a dataframe
-    '''
-    with open(filepath) as f:
-        lines = f.readlines()
-    
-    # Identify the location of the table
-    start_indx = None
-    for i, line in enumerate(lines):
-        if 'Group "Undervalued"' in line:
-            start_indx = i+3
-            break
+    all_files = []
 
-    end_indx = None
-    for j in range(start_indx+1, len(lines)):
-        if lines[j].strip()=='':
-            end_indx = j-1
-            break
+    for root, dirs, files in os.walk(folder):
+        files = glob.glob(os.path.join(root,'*.csv'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
 
-    # Extract the table
-    data = ''.join(lines[start_indx:end_indx])
-    df = pd.read_csv(StringIO(data))
-
-    # Transform the dataframe
-    df.dropna(subset=['BP Effect'], inplace=True)
-    df.reset_index(drop=True, inplace=True)
-    df = df[['Instrument','Qty','Trade Price']]
-    df.rename(columns={'Instrument':'Symbol'}, inplace=True)
-
-    return df
+    # get total number of files found
+    num_files = len(all_files)
+    return all_files, num_files
 
 # Transform new trades data
 def get_new_trades (filepath):
@@ -85,6 +55,44 @@ def get_new_trades (filepath):
     
     return df
 
+# Transform the current position data
+def get_current_pos(filepath):
+    '''
+    Processes the current position data downloaded from TOS.
+    Parameter is the filepath to the most current CSV file in "position_statement" folder.
+    1. Load the content of the CSV file.
+    2. Find and extract the table named "Undervalued"
+    3. Save the table as "current_pos.csv" in the parent folder
+    Return the table as a dataframe
+    '''
+    with open(filepath) as f:
+        lines = f.readlines()
+
+    # Identify the location of the table
+    start_indx = None
+    for i, line in enumerate(lines):
+        if 'Group "Undervalued"' in line:
+            start_indx = i + 3
+            break
+
+    end_indx = None
+    for j in range(start_indx + 1, len(lines)):
+        if lines[j].strip() == '':
+            end_indx = j - 1
+            break
+
+    # Extract the table
+    data = ''.join(lines[start_indx:end_indx])
+    df = pd.read_csv(StringIO(data))
+
+    # Transform the dataframe
+    df.dropna(subset=['BP Effect'], inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    df = df[['Instrument', 'Qty', 'Trade Price']]
+    df.rename(columns={'Instrument': 'Symbol'}, inplace=True)
+
+    return df
+
 # Get both previous and new undervalued trades
 def filter_new_trades(new_trades, pos_statement_file):
     '''
@@ -98,9 +106,8 @@ def filter_new_trades(new_trades, pos_statement_file):
     # Filter out overlapping stocks
     overlapping = pd.read_excel('overlapping_stocks.xlsx')
     overlapping.drop(columns='Strategy', inplace=True)
-
-    new_trades['Exec Date'] = pd.to_datetime(new_trades['Exec Time'],
-                                             format='%m/%d/%y %H:%M:%S').dt.normalize()  # Convert data from 'Exec Time' to datetime64[ns] and extract the dates
+    # Convert data from 'Exec Time' to datetime64[ns] and extract the dates
+    new_trades['Exec Date'] = pd.to_datetime(new_trades['Exec Time'], format='%m/%d/%y %H:%M:%S').dt.normalize()
     merged = pd.merge(new_trades, overlapping, how='left', on=['Exec Date', 'Symbol', 'Qty', 'Price'], indicator=True)
     new_trades = merged[merged['_merge'] == 'left_only'].drop(columns=['_merge', 'Exec Date'])
 
@@ -127,55 +134,53 @@ def update_undervalued_trades(previous_trades, new_undervalued_trades):
     Return all undervalued stock trades
     '''
 
+    # Make sure new rows don't exist in previous_trades
+    columns = ['Exec Time', 'Side', 'Pos Effect', 'Symbol', 'Qty', 'Price']
+    rows_to_add = pd.merge(new_undervalued_trades, previous_trades, on=columns, how='left', indicator=True)
+    rows_to_add = rows_to_add[rows_to_add['_merge'] == 'left_only'].drop(columns='_merge')
+
     # Add new records to all undervalued_trades.csv
-    rows_to_add = new_undervalued_trades[~new_undervalued_trades['Exec Time'].isin(previous_trades['Exec Time'])]
     trades = pd.concat([previous_trades, rows_to_add])
     trades.reset_index(drop=True, inplace=True)
 
     return trades
 
 # Process and update undervalued trades
-def main(acc_statement_file, pos_statement_file):
+def main():
     '''
     Apply other functions to update undervalued trades and save all trades to undervalued_trades.csv
+    1. Get all file paths in "account_statement" and "position_statement"
+    2. Use the first file in "account_statement" to generate "undervalued_trades.csv"
+    3. Iterates over the remaining account statements, pairing each with the corresponding position statement file.
+        - Filters new undervalued trades.
+        - Updates the 'undervalued_trades.csv' file with any new undervalued trades.
     '''
-    new_trades = get_new_trades(acc_statement_file)
-    previous_trades, new_undervalued_trades = filter_new_trades(new_trades, pos_statement_file)
-    trades = update_undervalued_trades(previous_trades, new_undervalued_trades)
-    # Save all trades to undervalued_trades.csv
-    trades.to_csv('undervalued_trades.csv', index=False)
+
+    acc_files, acc_num_files = get_files("account_statement")
+    print(f'{acc_num_files} files found in "account_statement"')
+
+    pos_files, pos_num_files = get_files("position_statement")
+    print(f'{pos_num_files} files found in "position_statement"')
+
+    for i in range(len(acc_files)):
+        acc_filepath = acc_files[i]
+        acc_file_name = os.path.basename(acc_filepath)
+        if i == 0:
+            first_trades = get_new_trades(acc_filepath)
+            first_trades.drop(8, inplace=True)
+            first_trades.to_csv('undervalued_trades.csv', index=False)
+            print(f'Created "undervalued_trades.csv" using "{acc_file_name}"')
+        else:
+            pos_filepath = pos_files[i - 1]
+            pos_file_name = os.path.basename(pos_filepath)
+            new_trades = get_new_trades(acc_filepath)
+            previous_trades, new_undervalued_trades = filter_new_trades(new_trades, pos_filepath)
+            trades = update_undervalued_trades(previous_trades, new_undervalued_trades)
+            # Save all trades to undervalued_trades.csv
+            trades.to_csv('undervalued_trades.csv', index=False)
+            print('Updated "undervalued_trades.csv"')
+            print(f'Files used: {acc_file_name}, {pos_file_name}')
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--pos_statement', help='Optional: A specific file name in position_statement folder')
-    parser.add_argument('--acc_statement', help='Optional: A specific file name in account_statement folder')
-    args = parser.parse_args()
-
-    # If acc_statement_file is not specified, get_latest_pos_statement will be executed to return the latest file
-    if args.acc_statement:
-        acc_statement_path = os.path.join('account_statement', args.acc_statement)
-    else:
-        choice = input("Use latest account statement file? (y/n): ").lower()
-
-        if choice == 'y':
-            acc_statement_path = get_latest_file('account_statement')
-        else:
-            acc_file = input("Enter file name: ").strip()
-            acc_statement_path = os.path.join('account_statement', acc_file)
-
-    # If pos_statement_file is not specified, get_latest_pos_statement will be executed to return the latest file
-    if args.pos_statement:
-        pos_statement_path = os.path.join('position_statement', args.pos_statement)
-    else:
-        choice = input("Use latest position statement file? (y/n): ").lower()
-
-        if choice == 'y':
-            pos_statement_path = get_latest_file('position_statement')
-        else:
-            pos_file = input("Enter file name: ").strip()
-            pos_statement_path = os.path.join('position_statement', pos_file)
-
-    main(acc_statement_path, pos_statement_path)
-    
-    print(f'Files used: {acc_statement_path}, {pos_statement_path}')
+    main()
