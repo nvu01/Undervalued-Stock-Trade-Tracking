e63b08cad4c5ba001497eaa7fe0aca65ff9c4cf5
nvu01 <nganvu2601@gmail.com>
2026-02-26 22:40:15 -0500

    Updated etl.py. Added function to update symbol change

--- etl.py
+++ etl.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 from io import StringIO
 
-# Get all account statements or position statements
+# Get all account statements or position statements 
 def get_files(folder):
     '''
     Retrieve all files in a folder
@@ -20,11 +20,11 @@ def get_files(folder):
     num_files = len(all_files)
     return all_files, num_files
 
-# Transform new trades data
-def get_new_trades (filepath):
+# Retrieve all trade data in a specific period
+def get_all_trades (filepath):
     '''
-    Processes the new trades data downloaded from TOS.
-    Parameter is the filepath to most current CSV file in "account_statement" folder.
+    Process account statements downloaded from TOS.
+    Parameter is the filepath to a CSV file in "account_statement" folder.
     1. Load the content of the CSV file.
     2. Find and extract the table named "Account Trade History"
     Return the table as a dataframe
@@ -54,7 +54,7 @@ def get_new_trades (filepath):
     
     return df
 
-# Transform the current position data
+# Retrieve and transform the current position data
 def get_current_pos(filepath):
     '''
     Processes the current position data downloaded from TOS.
@@ -92,55 +92,65 @@ def get_current_pos(filepath):
 
     return df
 
-# Get both previous and new undervalued trades
-def filter_new_trades(new_trades, pos_statement_file):
+# Use previous trades and position statement to get new trades
+def filter_new_trades(previous_trades, all_trades, pos_stmt_file):
     '''
-    Extract the undervalued stock trades from the new trades
-    1. Filter out other portfolios' trades for stocks that are also in the "Undervalued" portfolio
-    2. Extract buy trades for undervalued stocks based on current positions
-    3. Extract closing trades for underavalued stocks based on previous trades
-    Return previous trades and new trades
+    Extract the undervalued stock trades from the all the new trades
+    1. Extract buy trades for undervalued stocks based on current positions
+    2. Extract closing trades for underavalued stocks based on previous trades
+    Return new undervalued trades
     '''
-    # Filter out overlapping stocks
-    overlapping = pd.read_excel('overlapping_stocks.xlsx')
-    overlapping.drop(columns='Strategy', inplace=True)
-    # Convert data from 'Exec Time' to datetime64[ns] and extract the dates
-    new_trades['Exec Date'] = pd.to_datetime(new_trades['Exec Time'], format='%m/%d/%y %H:%M:%S').dt.normalize()
-    merged = pd.merge(new_trades, overlapping, how='left', on=['Exec Date', 'Symbol', 'Qty', 'Price'], indicator=True)
-    new_trades = merged[merged['_merge'] == 'left_only'].drop(columns=['_merge', 'Exec Date'])
-
     # Split the new trades into buy and sell trades
-    buy_to_open = new_trades[(new_trades['Side'] == 'BUY') & (new_trades['Pos Effect'] == 'TO OPEN')]
-    sell_to_close = new_trades[(new_trades['Side'] == 'SELL') & (new_trades['Pos Effect'] == 'TO CLOSE')]
+    buy_to_open = all_trades[(all_trades['Side'] == 'BUY') & (all_trades['Pos Effect'] == 'TO OPEN')]
+    sell_to_close = all_trades[(all_trades['Side'] == 'SELL') & (all_trades['Pos Effect'] == 'TO CLOSE')]
 
     # List of current positions is used to filter buy trades
-    current_position = get_current_pos(pos_statement_file)
+    current_position = get_current_pos(pos_stmt_file)
     undervalued_buy = buy_to_open[buy_to_open['Symbol'].isin(current_position['Symbol'])]
 
     # List of previous undervalued stock trades is used to filter sell-to-close trade
-    previous_trades = pd.read_csv('undervalued_trades.csv')
     filter_stocks = previous_trades['Symbol'].unique()
     undervalued_sell = sell_to_close[sell_to_close['Symbol'].isin(filter_stocks)]
     new_undervalued_trades = pd.concat([undervalued_buy, undervalued_sell])
 
-    return previous_trades, new_undervalued_trades
-
-# Update undervalued trades
-def update_undervalued_trades(previous_trades, new_undervalued_trades):
-    '''
-    Add new trades to previous trades
-    Return all undervalued stock trades
-    '''
     # Make sure new rows don't exist in previous_trades
     columns = ['Exec Time', 'Side', 'Pos Effect', 'Symbol', 'Qty', 'Price']
     rows_to_add = pd.merge(new_undervalued_trades, previous_trades, on=columns, how='left', indicator=True)
     rows_to_add = rows_to_add[rows_to_add['_merge'] == 'left_only'].drop(columns='_merge')
 
-    # Add new records to all undervalued_trades.csv
-    trades = pd.concat([previous_trades, rows_to_add])
-    trades.reset_index(drop=True, inplace=True)
+    return rows_to_add
 
-    return trades
+def remove_overlapping_stocks(trades):
+    '''
+    Filter out other portfolios' trades for stocks that are also in the "Undervalued" portfolio
+    '''
+    # Filter out overlapping stocks
+    overlapping = pd.read_excel('overlapping_stocks.xlsx')
+    overlapping.drop(columns='Strategy', inplace=True)
+    # Convert data from 'Exec Time' to datetime64[ns] and extract the dates
+    trades['Exec Date'] = pd.to_datetime(trades['Exec Time'], errors='coerce').dt.normalize()
+    merged = pd.merge(trades, overlapping, how='left', on=['Exec Date', 'Symbol', 'Qty', 'Price'], indicator=True)
+    filtered_trades = merged[merged['_merge'] == 'left_only'].drop(columns=['_merge', 'Exec Date'])
+
+    return filtered_trades
+
+def update_changes(trades):
+    '''
+    Update changes in ticker symbol
+    1. Get the new changes from "Symbol Change.xlsx"
+    2. Remove records of old symbols
+    3. Add new changes to the trade data
+    '''
+    # Import the Excel file that stores changes
+    ticker_change = pd.read_excel('Symbol Change.xlsx')
+    # Find new changes that are not yet updated in trades
+    replace_rows = ticker_change.loc[ticker_change['Old Symbol'].isin(trades['Symbol']), ['Exec Time', 'Side', 'Pos Effect', 'Symbol', 'Qty','Price']]
+    # Remove the records of old symbols
+    trades = trades[~trades['Symbol'].isin(ticker_change['Old Symbol'])]
+    # Add new updated records to trades
+    updated_trades = pd.concat([trades, replace_rows])
+
+    return updated_trades
 
 # Process and update undervalued trades
 def main():
@@ -158,25 +168,35 @@ def main():
     pos_files, pos_num_files = get_files("position_statement")
     print(f'{pos_num_files} files found in "position_statement"')
 
+    previous_trades = pd.DataFrame()
+
     for i in range(len(acc_files)):
         acc_filepath = acc_files[i]
         acc_file_name = os.path.basename(acc_filepath)
+
+        all_trades = get_all_trades(acc_filepath)
+
         if i == 0:
-            first_trades = get_new_trades(acc_filepath)
-            first_trades.drop(8, inplace=True)
-            first_trades.to_csv('undervalued_trades.csv', index=False)
-            print(f'Created "undervalued_trades.csv" using "{acc_file_name}"')
+            new_trades = all_trades.drop(8)
+            print(f'First trades retrieved. File used: {acc_file_name}')
         else:
             pos_filepath = pos_files[i - 1]
             pos_file_name = os.path.basename(pos_filepath)
-            new_trades = get_new_trades(acc_filepath)
-            previous_trades, new_undervalued_trades = filter_new_trades(new_trades, pos_filepath)
-            trades = update_undervalued_trades(previous_trades, new_undervalued_trades)
-            # Save all trades to undervalued_trades.csv
-            trades.to_csv('undervalued_trades.csv', index=False)
-            print('Updated "undervalued_trades.csv"')
-            print(f'Files used: {acc_file_name}, {pos_file_name}')
 
+            new_trades = filter_new_trades(previous_trades, all_trades, pos_filepath)
+            print(f'Data batch #{i} retrieved. Files used: {acc_file_name}, {pos_file_name}')
+
+        # Update running DataFrame
+        previous_trades = pd.concat([previous_trades, new_trades], ignore_index=True)
+
+    all_new_trades = previous_trades
+    filtered_trades = remove_overlapping_stocks(all_new_trades)
+    updated_trades = update_changes(filtered_trades)
+
+    # Save all trades to undervalued_trades.csv
+    updated_trades.to_csv('undervalued_trades.csv', index=False)
+
+    print('Data update complete')
 
 if __name__ == "__main__":
     main()
